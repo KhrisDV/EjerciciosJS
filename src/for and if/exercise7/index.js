d25efc8f81ad0538cfd57717af85d85e56b06d3e
KhrisDV <KhrisQ@protonmail.com>
2022-02-22 12:41:00 +0100

    ex 7 & ex 8 done

--- src/for and if/exercise7/index.js
+++ src/for and if/exercise7/index.js
@@ -1,4 +1,5 @@
 const numbers = ["Frodo","Gandalf","Turin","Sauron","Saruman","Bilbo"];
 
-// Put your code here
-
+for(let idx=3;idx<6;idx++){
+        console.log(numbers[idx])
+}
--- src/for and if/exercise8/index.js
+++ src/for and if/exercise8/index.js
@@ -1,1 +1,7 @@
-// Put your code here
+   for (let inner = 1; inner < 10; inner++) {
+       for (let outer = 1; outer < 10; outer++){
+        console.log(`Multiplicar: ${outer} x ${inner}`);
+       }
+   }
+   console.log("\n")
+        
