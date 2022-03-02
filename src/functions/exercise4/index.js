294a2dd41e71692afdd2dfbffa791cdbbf14ba2b
KhrisDV <KhrisQ@protonmail.com>
2022-03-02 13:10:07 +0100

    ex 4 & 5 done

--- src/functions/exercise4/index.js
+++ src/functions/exercise4/index.js
@@ -1,9 +1,14 @@
-const numbers = [11,12,13,14]
+const numbers = [11, 12, 13, 14];
+console.log(numbers);
+function swap(number1, number2) {
+  let temp = numbers[number2];
+  numbers[number2] = numbers[number1];
+  console.log(numbers[0]);
+  numbers[number1] = temp;
+}
 
-// Put your code here
-
-swap(0,3,numbers);
+swap(0, 3, numbers);
 console.log(numbers);
 
-swap(1,2,numbers);
+swap(1, 2, numbers);
 console.log(numbers);
--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -1,7 +1,18 @@
-const numbers = [11,9,13,12]
+const numbers = [11, 9, 13, 12];
 
-// Put your code here
+function minorInRange(pos1, pos2, arraynumbers) {
+  let menor = arraynumbers[pos1];
+  for (let i = pos1; i < pos2; i++) {
+    if (arraynumbers[i] < arraynumbers[i + 1]) {
+      menor = arraynumbers[i];
+    }
+  }
+  if (arraynumbers[pos2] < menor) {
+    menor = arraynumbers[pos2];
+  }
+  return menor;
+}
 
-console.log(minorInRange(0,3,numbers));
-console.log(minorInRange(2,3,numbers));
-console.log(minorInRange(1,3),numbers);
+console.log(minorInRange(0, 3, numbers));
+console.log(minorInRange(2, 3, numbers));
+console.log(minorInRange(1, 3, numbers));
